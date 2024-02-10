020bd8f01e32e34bece0990149715eec326e2be2
ekohadipermana <ekohadipermana@gmail.com>
2024-02-10 16:39:10 +0700

    creating game logic

--- script.js
+++ script.js
@@ -6,13 +6,29 @@ document.querySelector('.guess').value = 20;
 console.log(document.querySelector('.guess').value);
 */
 // creating a random number between 1 and 20
-let randomNumber = Math.random() * 20 + 1;
-
+let randomNumber = Math.floor(Math.random() * 20 + 1);
+document.querySelector('.number').textContent = randomNumber;
+let score = 20;
 
 document.querySelector('.check').addEventListener('click', function () {
     console.log(document.querySelector('.guess').value);
     let guess = Number(document.querySelector('.guess').value);
     if (!guess) {
         document.querySelector('.message').textContent = '⚠ No Number ⚠';
-    };
+    }
+    else if (guess > randomNumber) {
+        document.querySelector('.message').textContent = 'Too high';
+        score -= 1;
+        document.querySelector('.score').textContent = score;
+    }
+    else if (guess < randomNumber) {
+        document.querySelector('.message').textContent = 'Too low';
+        score -= 1;
+        document.querySelector('.score').textContent = score;
+    }
+    else if (guess === randomNumber) {
+        document.querySelector('.message').textContent = 'Bingo!';
+    }
+    
 });
+
