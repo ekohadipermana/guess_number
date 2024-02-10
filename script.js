6615b82334f276b953d879204439d81357631f4d
ekohadipermana <ekohadipermana@gmail.com>
2024-02-10 09:56:26 +0700

    commenting out previous practice

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 'use strict';
-
+/*
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = 'Correct Number';
 document.querySelector('.guess').value = 20;
 console.log(document.querySelector('.guess').value);
+*/
