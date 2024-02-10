5afa91087a231af211792e508c963be2944f8bca
ekohadipermana <ekohadipermana@gmail.com>
2024-02-10 19:46:40 +0700

    creating the logic using while loop

--- script2.js
+++ script2.js
@@ -1,1 +1,22 @@
 'use strict';
+let score = 20;
+let theNumber = 5;
+// document.querySelector('.score').textContent;
+theGuess = Number(document.querySelector('.guess').textContent);
+
+while (score > 0) {
+    if (theGuess > theNumber) {
+        document.querySelector('.message').textContent = 'Too High';
+        score--;
+        document.querySelector('.score').textContent = score;
+    }
+    else if (theGuess < theNumber) {
+        document.querySelector('.message').textContent = 'Too Low';
+        score--;
+        document.querySelector('.score').textContent = score;
+    }
+    else if (theGuess === theNumber) {
+        document.querySelector('.message').textContent = 'Bingo';
+    }
+}
+document.querySelector('.message').textContent = 'You lose';
