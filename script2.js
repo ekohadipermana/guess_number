c191bf67480197a53fdc9e9ec3302a2f7146739c
ekohadipermana <ekohadipermana@gmail.com>
2024-02-10 21:39:52 +0700

    creating the guess

--- script2.js
+++ script2.js
@@ -1,49 +1,24 @@
-'use strict';
-let score = 20;
-document.querySelector('.score').textContent = score;
-let theNumber = 5;
-let theGuess = Number(document.querySelector('.guess').value);
-// console.log(theGuess);
+console.log(document.querySelector('.number').textContent);
+console.log(document.querySelector('.guess').value);
+let clicked = function() {
+    console.log(document.querySelector('.message').textContent = 'clicked');
+}
+// document.querySelector('.check').addEventListener('click', clicked);
+console.log(document.querySelector('.score').textContent)
 
-let checkGuess = function () {
+let theNumber = 10;
+let score = 5;
+let checkGuess = function() {
+    
     while (score > 0) {
-        // console.log(score);
-        // score--;
+        document.querySelector('.score').textContent = score;
+        theGuess = Number(document.querySelector('.guess').value);
         if (theGuess > theNumber) {
-            console.log('too high');
-            score--;
-        } 
+            document.querySelector('.message').textContent = 'Too high';
+            
+        }
+        score--;
     }
 
 }
 document.querySelector('.check').addEventListener('click', checkGuess);
-
-
-// document.querySelector('.score').textContent;
-
-// document.querySelector('.number').textContent = theNumber;
-
-
-
-
-
-
-
-/*
-while (score > 0) {
-    if (theGuess > theNumber) {
-        document.querySelector('.message').textContent = 'Too High';
-        score--;
-        document.querySelector('.score').textContent = score;
-    }
-    else if (theGuess < theNumber) {
-        document.querySelector('.message').textContent = 'Too Low';
-        score--;
-        document.querySelector('.score').textContent = score;
-    }
-    else if (theGuess === theNumber) {
-        document.querySelector('.message').textContent = 'Bingo';
-    }
-}
-document.querySelector('.message').textContent = 'You lose';
-*/
