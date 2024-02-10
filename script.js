3ac2c529d3d54f8df3f90d24cadb905eb2553a59
ekohadipermana <ekohadipermana@gmail.com>
2024-02-10 19:03:50 +0700

    adding alternative logic checks

--- script.js
+++ script.js
@@ -10,25 +10,91 @@ let randomNumber = Math.floor(Math.random() * 20 + 1);
 document.querySelector('.number').textContent = randomNumber;
 let score = 20;
 
-document.querySelector('.check').addEventListener('click', function () {
-    console.log(document.querySelector('.guess').value);
+// first alternative
+let checkGuess = function () {
+    // console.log(document.querySelector('.guess').value);
     let guess = Number(document.querySelector('.guess').value);
-    if (!guess) {
-        document.querySelector('.message').textContent = '⚠ No Number ⚠';
+    // document.querySelector('.score').textContent = score;
+    if (score < 1) {
+        document.querySelector('.message').textContent = '⛔You lose';
     }
-    else if (guess > randomNumber) {
-        document.querySelector('.message').textContent = 'Too high';
-        score -= 1;
-        document.querySelector('.score').textContent = score;
+    else {
+        if (!guess) {
+            document.querySelector('.message').textContent = '⚠ No Number ⚠';
+        }
+        else if (guess > randomNumber) {
+            document.querySelector('.message').textContent = 'Too high';
+            score -= 1;
+            document.querySelector('.score').textContent = score;    
+        }
+        else if (guess < randomNumber) {
+            document.querySelector('.message').textContent = 'Too low';
+            score -= 1;
+            document.querySelector('.score').textContent = score; 
+        }
+        else if (guess === randomNumber) {
+            document.querySelector('.message').textContent = 'Bingo!';
+        }
     }
-    else if (guess < randomNumber) {
-        document.querySelector('.message').textContent = 'Too low';
-        score -= 1;
-        document.querySelector('.score').textContent = score;
+}
+
+// Second Alternative
+let checkGuess2 = function () {
+    // console.log(document.querySelector('.guess').value);
+    let guess = Number(document.querySelector('.guess').value);
+    // document.querySelector('.score').textContent = score;
+    if (score > 1) {
+        if (!guess) {
+            document.querySelector('.message').textContent = '⚠ No Number ⚠';
+        }
+        else if (guess > randomNumber) {
+            document.querySelector('.message').textContent = 'Too high';
+            score -= 1;
+            document.querySelector('.score').textContent = score;    
+        }
+        else if (guess < randomNumber) {
+            document.querySelector('.message').textContent = 'Too low';
+            score -= 1;
+            document.querySelector('.score').textContent = score; 
+        }
+        else if (guess === randomNumber) {
+            document.querySelector('.message').textContent = 'Bingo!';
+        }
     }
-    else if (guess === randomNumber) {
-        document.querySelector('.message').textContent = 'Bingo!';
+    else {
+        document.querySelector('.message').textContent = '⛔You lose';
     }
-    
+}
+
+document.querySelector('.check').addEventListener('click', checkGuess2);
+
+/*
+document.querySelector('.check').addEventListener('click', function () {
+    // console.log(document.querySelector('.guess').value);
+    let guess = Number(document.querySelector('.guess').value);
+    // document.querySelector('.score').textContent = score;
+    if (score < 1) {
+        document.querySelector('.message').textContent = '⛔You lose';
+    }
+    else {
+        if (!guess) {
+            document.querySelector('.message').textContent = '⚠ No Number ⚠';
+        }
+        else if (guess > randomNumber) {
+            document.querySelector('.message').textContent = 'Too high';
+            score -= 1;
+            document.querySelector('.score').textContent = score;    
+        }
+        else if (guess < randomNumber) {
+            document.querySelector('.message').textContent = 'Too low';
+            score -= 1;
+            document.querySelector('.score').textContent = score; 
+        }
+        else if (guess === randomNumber) {
+            document.querySelector('.message').textContent = 'Bingo!';
+        }
+    }    
 });
+*/
+
 
