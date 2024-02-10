70200160e8dff815530f982e356317d17dde1cd5
ekohadipermana <ekohadipermana@gmail.com>
2024-02-10 19:21:55 +0700

    creating second script for alternative

--- script.js
+++ script.js
@@ -63,10 +63,35 @@ let checkGuess2 = function () {
     }
     else {
         document.querySelector('.message').textContent = '⛔You lose';
+        document.querySelector('.score').textContent = score;
     }
 }
 
-document.querySelector('.check').addEventListener('click', checkGuess2);
+// Third alternative
+let checkGuess3 = function () {
+    let guess = Number(document.querySelector('.guess').value);
+    while (score > 0) {
+        if (!guess) {
+            document.querySelector('.message').textContent = '⚠ No Number ⚠';
+        }
+        else if (guess > randomNumber) {
+            document.querySelector('.message').textContent = 'Too high';
+            score -= 1;
+            document.querySelector('.score').textContent = score;    
+        }
+        else if (guess < randomNumber) {
+            document.querySelector('.message').textContent = 'Too low';
+            score -= 1;
+            document.querySelector('.score').textContent = score; 
+        }
+        else if (guess === randomNumber) {
+            document.querySelector('.message').textContent = 'Bingo!';
+        }
+    }
+    document.querySelector('.message').textContent = '⛔You lose';
+}
+
+document.querySelector('.check').addEventListener('click', checkGuess3);
 
 /*
 document.querySelector('.check').addEventListener('click', function () {
