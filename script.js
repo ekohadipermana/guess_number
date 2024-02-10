fb046c47a68e379951e909fa548fc401a3580d74
ekohadipermana <ekohadipermana@gmail.com>
2024-02-10 09:55:45 +0700

    trying basic DOM

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 'use strict';
 
-console.log(document.querySelector('.message'));
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number';
+document.querySelector('.guess').value = 20;
+console.log(document.querySelector('.guess').value);
